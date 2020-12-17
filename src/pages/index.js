ad2c1b3a6f8ebcbbe9f127130417067e26f4374d
hithesh svsk <8svskhd@gmail.com>
2020-12-17 15:36:06 +0530

    second

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,62 @@
 import React from "react"
-import { Link } from "gatsby"
+import Layout from "../components/layout/Layout"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../styles.css"
+import {FaCarSide,FaCaravan,FaShuttleVan,FaWhatsapp} from "react-icons/fa"
+import {Container,Row,Col,Button} from "reactstrap"
+import Type from "../components/Typed";
+import Cardx from "../components/Card";
+import Toggle from "../components/Toggle";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const year = new Date().getFullYear()
+const day = new Date().getDate()
+// time
+const hours = new Date().getHours()
+const Minutes = new Date().getMinutes() 
 
 const IndexPage = () => (
+  
+  <div>
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+      <div>
+        <Container>
+          <Row>
+          
+          <Col xs="12" md="6" lg="6">
+          <Cardx
+          title={"Welcome To Locomotive services in Tirupati"}
+          body={`${day}/${year}`}
+          Time={`${hours}:${Minutes}`}
+          />
+          <h1 className="display-4"><b>Avail our Services for</b></h1>
+          <div className="display-3"><b><Type/></b></div>
+          
+          </Col>
+         
+          <Col xs="12" md="6" lg="6">
+          <div className="icons">
+          <div className="firsticon"><Button outline color="dark"><FaCarSide size={100}/></Button></div>
+          <div className="secondicon"><Button outline color="dark"><FaShuttleVan size={100}/></Button></div>
+          <div className="thirdicon"><Button outline color="dark"><FaCaravan size={100}/></Button></div>
+          </div>
+          </Col>
+         </Row>
+         </Container>
+         <hr/>
+         <Container>
+           <Row>
+           <Col>
+           <Toggle/>
+           </Col>
+           <Col>
+           <h3>Contact us</h3>
+           <Button size="sm" href="https://wa.me/9553419654" target="_blank" outline color="dark"><FaWhatsapp size={30}/></Button>
+           </Col>
+           </Row>
+          </Container>
+      </div>
+ </Layout>
+ </div>
 )
 
 export default IndexPage
